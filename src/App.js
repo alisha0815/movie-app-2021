6af1bea0310b337ac14eab007baf13cb4dbc4b92
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-09-06 17:17:11 +0200

    reusable components with JSX + props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 // import logo from "./logo.svg";
 // import "./App.css";
 import React from "react";
-import Potato from "./Potato";
+// props go to the first argument of the function
+function Food({ fav }) {
+  console.log(fav);
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello!</h1>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="spagetti" />
+      {/* This is how to pass the information */}
+      {/*prop name fav = kimchi */}
     </div>
   );
 }
