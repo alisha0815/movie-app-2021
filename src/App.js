ea767e81a566fc6378cc04239680535fb7ee6f38
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-09-06 22:47:11 +0200

    fetching movies with API async

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
+import axios from "axios";
 // Every time you call setState, react is going to rerender with the new state
 
 // If we don't use setState, render function wouldn't be called with the new state
 class App extends React.Component {
   state = {
     isLoading: true, // default is loading
+    movies: [],
   };
 
+  getMovies = async () => {
+    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
+  };
   componentDidMount() {
-    //   delay function
-    setTimeout(() => {
-      setTimeout(() => {
-        this.setState({ isLoading: false });
-      }, 4000);
-    });
+    this.getMovies();
   }
   render() {
     const { isLoading } = this.state; // return return{" "}
