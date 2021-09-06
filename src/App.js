78dc301f44bc577280b9c3a85fa39efcff00f015
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-09-06 23:13:06 +0200

    rendering movies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 // Every time you call setState, react is going to rerender with the new state
 
 // If we don't use setState, render function wouldn't be called with the new state
@@ -10,14 +11,40 @@ class App extends React.Component {
   };
 
   getMovies = async () => {
-    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+    );
+    // console.log(movies.data.data.movies);
+    console.log(movies);
+    this.setState({ movies: movies, isLoading: false });
   };
   componentDidMount() {
     this.getMovies();
   }
   render() {
-    const { isLoading } = this.state; // return return{" "}
-    return <div>{isLoading ? "Loading...." : "We are ready 🤗"}</div>;
+    const { isLoading, movies } = this.state; // return return{" "}
+    return (
+      <div>
+        {isLoading
+          ? "Loading...."
+          : movies.map((movie) => (
+              //   console.log(movie);
+
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+      </div>
+    );
   }
 }
 
